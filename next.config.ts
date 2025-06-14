4e820d3a6ecff7da4839cde631692ea4bc8aef5e
Glauk Thaqi <glaukthaqi15@gmail.com>
2025-06-14 16:54:10 +0400

    logs removable

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+};
 
 export default withNextIntl(nextConfig);
